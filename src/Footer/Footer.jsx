d500f19dd5f2ff71b3ca9e876ace18e946aabba3
Veronika Ertel <veronika.ertel@gmail.com>
2024-01-26 16:08:51 +0200

    Footer component, id replaced with const

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,11 +1,12 @@
 // import Row from 'react-bootstrap/Row';
 import TargetLow from './TargetLow';
 import TargetHigh from './TargetHigh';
-
+import { DEFAULT_ACTIVE_BUTTON } from '../Head/constants'; 
 
 function Footer({ activePrice }) {
     const getSelectedFooterComponent = () => {
-        return activePrice === 'high' ? <TargetHigh /> : <TargetLow />;
+        // return activePrice === 'high' ? <TargetHigh /> : <TargetLow />;
+        return activePrice === DEFAULT_ACTIVE_BUTTON ? <TargetLow /> : <TargetHigh />;
     };
 
     return (
