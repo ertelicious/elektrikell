addb3e7099df98bf9dd7919a1ea6b094de52e84d
Veronika Ertel <veronika.ertel@gmail.com>
2024-01-26 14:42:05 +0200

    footer info changes on click

--- src/Footer/Footer.jsx
+++ src/Footer/Footer.jsx
@@ -1,7 +1,18 @@
-import Row from 'react-bootstrap/Row';
+// import Row from 'react-bootstrap/Row';
+import TargetLow from './TargetLow';
+import TargetHigh from './TargetHigh';
 
-function Footer() {
-    return (<></>);
+
+function Footer({ activePrice }) {
+    const getSelectedFooterComponent = () => {
+        return activePrice === 'high' ? <TargetHigh /> : <TargetLow />;
+    };
+
+    return (
+        <>
+            {getSelectedFooterComponent()}
+        </>
+    );
 }
 
 export default Footer;
