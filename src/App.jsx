99c416ccdc62c11488af83cb1c82ce6e393a08ff
Veronika Ertel <veronika.ertel@gmail.com>
2024-02-12 18:22:42 +0200

    about pages in class

--- src/App.jsx
+++ src/App.jsx
@@ -1,37 +1,25 @@
-import { Routes, Route } from 'react-router-dom';
-import ElectricPrice from './ElectricPrice';
-import About from './About/About';
-import Navigation from './Navigation';
-
+import { Routes, Route } from "react-router-dom";
+import ElectricPrice from "./ElectricPrice";
+import About from "./About/About";
+import Navigation from "./Navigation";
 
 function App() {
     return (
         <>
             <Navigation />
             <Routes>
-                <Route 
+                <Route
                     path="/" // homepage
                     element={<ElectricPrice />} // какой компонент будет homepage'ом
                 />
-                    <Route 
-                        path="lowprice/:hours"
-                        element={<ElectricPrice />}
-                    />
-                <Route 
-                    path="/about" 
-                    element={<About />}
-                />
+                <Route path="lowprice/:hours" element={<ElectricPrice />} />
+                <Route path="/about" element={<About />} />
                 {/* делаем id. для этого нам нужно два роута */}
-                <Route 
-                    path="/about/:id" 
-                    element={<About />}
-                />
+                <Route path=":who" element={<About />} />
+
                 {/* 404 error */}
-                <Route 
-                    path="*" 
-                    element={<h1>404</h1>} 
-                />
-            </Routes> 
+                <Route path="*" element={<h1>404</h1>} />
+            </Routes>
         </>
     );
 }
