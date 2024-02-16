2cd78394bcc5621f05e6275199b2f948b1fe99f5
Veronika Ertel <veronika.ertel@gmail.com>
2024-02-16 10:55:46 +0200

    price level badge

--- src/Head/Info.jsx
+++ src/Head/Info.jsx
@@ -2,7 +2,7 @@ import Badge from 'react-bootstrap/Badge';
 import Col from 'react-bootstrap/Col';
 import ButtonGroup from 'react-bootstrap/ButtonGroup';
 import Button from 'react-bootstrap/Button';
-import { PRICE_BUTTONS, BADGES, ERROR_MESSAGE } from './constants';
+import { PRICE_BUTTONS, BADGES, ERROR_MESSAGE, LOW, HIGH } from './constants';
 import { useEffect, useState, useContext } from 'react';
 import { getCurrentPrice } from '../services/apiService';
 import { mwToKw, addTax } from '../utils/priceFormat';
@@ -19,6 +19,7 @@ function Info () {
     console.log(values.averagePrice);
 
     const [currentPrice, setCurrentPrice] = useState(0);
+    const [priceLevel, setPriceLevel] = useState(0);
     const activePrice = useSelector((state) => state.main.activePrice); //redux
 
     useEffect(() => {
@@ -29,35 +30,44 @@ function Info () {
                 if(!success) throw new Error();
 
                 setCurrentPrice(addTax(mwToKw(data[0].price), 'ee'));
+
+                if (currentPrice > 6) {
+                    setPriceLevel(HIGH);
+                } else {
+                    setPriceLevel(LOW);
+                }
+
             } catch (error) {
                 dispatch(setErrorMessage(ERROR_MESSAGE));
             }    
         }) ();
-    }, [dispatch]); // setErrorMessage это функция, кот. никогда не изменится, но useEffect все равно в данном случае просит dependency
+    }, [dispatch, currentPrice]); 
 
     return (
         <>
-        <Col className="mt-auto">
-            <div>Current price of electricity is</div>
-            <Badge bg={BADGES[0].name}>{BADGES[0].id}</Badge>
-        </Col>
+            <Col className="mt-auto">
+                <div>Current price of electricity is</div>
+                {/* <Badge bg={BADGES[0].name}>{BADGES[0].id}</Badge> */}
+                <Badge bg={priceLevel === BADGES[0].id ? 'success' : 'danger'}>{priceLevel}</Badge>
+            </Col>
 
-        <Col className="text-center mt-auto">
-            <ButtonGroup>
-                {PRICE_BUTTONS.map(({ name, id }) => (
-                <Button 
-                    key={id}
-                    active={activePrice === id} 
-                    onClick={() => dispatch(setActivePrice(id))} 
-                    variant="outline-warning">{name}</Button>
-                    ))}
-         </ButtonGroup>
-        </Col>
+            <Col className="text-center mt-auto">
+                <ButtonGroup>
+                    {PRICE_BUTTONS.map(({ name, id }) => (
+                    <Button 
+                        key={id}
+                        active={activePrice === id} 
+                        onClick={() => dispatch(setActivePrice(id))} 
+                        variant="outline-warning">{name}
+                    </Button>
+                        ))}
+            </ButtonGroup>
+            </Col>
 
-        <Col className="text-end">
-             <h2>{currentPrice}</h2>
-             <div>cent / kilowatt-hour</div>
-        </Col>
+            <Col className="text-end">
+                <h2>{currentPrice}</h2>
+                <div>cent / kilowatt-hour</div>
+            </Col>
         </>
     );
 }
